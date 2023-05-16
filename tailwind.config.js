febc7fa971c2e4f7206aa672ab47f7d0ac1502d1
Guilherme Guia <guilhermeguia007@gmail.com>
2023-05-16 16:29:39 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#111827',
+        secondary: '#1f2937',
+        purple: '#8257E5'
+      }
+    },
+  },
+  plugins: [],
+}
+
